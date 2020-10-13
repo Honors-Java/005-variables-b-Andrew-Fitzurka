7526afc0f595fc60588b83ae8dbbebbdfb9fb385
AndrewFitzurka1 <will_treaty@icloud.com>
2020-10-13 15:40:21 +0000

    Finished the assignment

--- index.java
+++ index.java
@@ -1,3 +1,8 @@
+int x = 150;
+int y = 150;
+int a = 275;
+int b = 100;
+
 void setup() {
 	size(500, 500);
 }
@@ -16,10 +21,21 @@ void draw() {
 
 // Step 4: Make it so when the program runs the ball slides off the screen exiting at the point (500, 500)
 
-  background(255);
+  background(60, 6, 79);
   stroke(0);
 
-
+  fill(200, 175, 56);
+  rectMode(CENTER);
+  rect(x, y, a, a);
+  fill(255, 25, 39);
+  ellipseMode(CENTER);
+  ellipse(x, y, b, b);
   
+  x = x + 1;
+  y = y + 1;
+}
 
+void mousePressed() {
+  a = a - 10;
+  b = b - 5;
 }
